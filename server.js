df43f08edc7fff8789977eb4889fa53b53956cd9
Laurynas Rup <lauryfas@gmail.com>
2021-04-18 13:51:18 +0100

    Port update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 const cors = require('cors');
 
 //Routes
