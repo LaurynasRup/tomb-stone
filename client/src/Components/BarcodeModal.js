fcf6d499f0225824ea297e45acb19dc8e065d8b5
Laurynas Rup <lauryfas@gmail.com>
2021-05-24 13:09:19 +0100

    Video size fix 2

--- client/src/Components/BarcodeModal.js
+++ client/src/Components/BarcodeModal.js
@@ -100,8 +100,8 @@ const MediaDiv = styled.div`
 
 	#interactive.viewport video {
 		width: 100%;
-		height: 300px;
-		transform: translateY(-25%);
+		height: 400px;
+		transform: translateY(-20%);
 	}
 
 	#interactive.viewport canvas {
