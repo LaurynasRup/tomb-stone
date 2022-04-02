f0f5849fc4c0f473f8dbcac7b285dd249fe0070e
laurynasrup <rupainis.laurynas@gmail.com>
2022-04-02 16:22:07 +0100

    Update type image url - display thumb

--- client/src/Components/AllTypesTable.js
+++ client/src/Components/AllTypesTable.js
@@ -6,68 +6,78 @@ import { BtnRedSm } from '../Components/Button';
 import { MdDelete } from 'react-icons/md';
 
 const AllTypesTable = ({ productDeleteHandler, typesArray }) => {
-	return (
-		<StyledTable>
-			<thead>
-				<tr>
-					<th>Name</th>
-					<th>Code</th>
-					<th>Image</th>
-					<th>Delete</th>
-				</tr>
-			</thead>
-			<tbody>
-				{typesArray.map((type) => (
-					<tr key={type._id}>
-						<td>{type.name}</td>
-						<td>{type.type_id}</td>
-						<td>
-							<img src={type.image} alt="Product Texture" />
-						</td>
-						<td>
-							<BtnRedSm
-								handler={() => productDeleteHandler(type.type_id, type._id)}
-							>
-								<MdDelete />
-							</BtnRedSm>
-						</td>
-					</tr>
-				))}
-			</tbody>
-		</StyledTable>
-	);
+  const displayThumbUrl = url => {
+    const thumbUrlArr = url.split('upload/');
+    const thumbUrl = `${thumbUrlArr[0]}upload/c_thumb,w_200,g_face/${thumbUrlArr[1]}`;
+    return thumbUrl;
+  };
+
+  return (
+    <StyledTable>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Code</th>
+          <th>Image</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        {typesArray.map(type => (
+          <tr key={type._id}>
+            <td>{type.name}</td>
+            <td>{type.type_id}</td>
+            <td>
+              <img
+                src={displayThumbUrl(type.image)}
+                alt="Product Texture"
+                loading="lazy"
+              />
+            </td>
+            <td>
+              <BtnRedSm
+                handler={() => productDeleteHandler(type.type_id, type._id)}
+              >
+                <MdDelete />
+              </BtnRedSm>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </StyledTable>
+  );
 };
 
 const StyledTable = styled.table`
-	border-collapse: collapse;
-	width: 100%;
-	min-width: 400px;
-	overflow-y: scroll;
-	th {
-		border-top: solid 1px #5c5c5c;
-		border-bottom: solid 1px #5c5c5c;
-		padding: 0.4rem;
-		text-align: left;
-		font-weight: 600;
-		background: #d6d6d6;
-		min-width: 100px;
-	}
+  border-collapse: collapse;
+  width: 100%;
+  min-width: 400px;
+  overflow-y: scroll;
+  th {
+    border-top: solid 1px #5c5c5c;
+    border-bottom: solid 1px #5c5c5c;
+    padding: 0.4rem;
+    text-align: left;
+    font-weight: 600;
+    background: #d6d6d6;
+    min-width: 100px;
+  }
 
-	tbody {
-		tr {
-			cursor: default;
-			transition: background 0.2s ease;
-		}
-	}
-	td {
-		border-bottom: solid 1px #5c5c5c;
-		padding: 0.4rem;
+  tbody {
+    tr {
+      cursor: default;
+      transition: background 0.2s ease;
+    }
+  }
+  td {
+    border-bottom: solid 1px #5c5c5c;
+    padding: 0.4rem;
 
-		img {
-			height: 25px;
-			width: 25px;
-			border-radius: 50%;
-		}
-	}
+    img {
+      height: 25px;
+      width: 25px;
+      border-radius: 50%;
+    }
+  }
 `;
 export default AllTypesTable;
--- routes/uploadImages.js
+++ routes/uploadImages.js
@@ -2,38 +2,38 @@ const router = require('express').Router();
 const { cloudinary } = require('../cloudinary/cloudinary');
 
 router.post('/', async (req, res) => {
-	try {
-		const fileStr = req.body.data;
-		const preset = req.body.upload_preset;
-		const uploadResponse = await cloudinary.uploader.upload(fileStr, {
-			upload_preset: preset,
-		});
-		const adjustedImgUrl = uploadResponse.secure_url.replace(
-			'upload/',
-			'upload/q_auto:low/'
-		);
-		res.json({
-			msg: 'Image uploaded succesfully',
-			data: adjustedImgUrl,
-		});
-	} catch (error) {
-		res.status(500).json({ msg: 'Failed to upload the image', error });
-	}
+  try {
+    const fileStr = req.body.data;
+    const preset = req.body.upload_preset;
+    const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+      upload_preset: preset,
+    });
+    const adjustedImgUrl = uploadResponse.secure_url.replace(
+      'upload/',
+      'upload/q_auto:low/'
+    );
+    res.json({
+      msg: 'Image uploaded succesfully',
+      data: adjustedImgUrl,
+    });
+  } catch (error) {
+    res.status(500).json({ msg: 'Failed to upload the image', error });
+  }
 });
 
 router.post('/destroy', async (req, res) => {
-	try {
-		const { public_id } = req.body;
-		if (!public_id) return res.status(400).json({ msg: 'No Images Selected' });
+  try {
+    const { public_id } = req.body;
+    if (!public_id) return res.status(400).json({ msg: 'No Images Selected' });
 
-		cloudinary.uploader.destroy(public_id, async (err, result) => {
-			if (err) throw err;
+    cloudinary.uploader.destroy(public_id, async (err, result) => {
+      if (err) throw err;
 
-			return res.status(200).json({ msg: 'Image Deleted Succesfully' });
-		});
-	} catch (error) {
-		return res.status(500).json({ msg: 'Failed to delete the image' });
-	}
+      return res.status(200).json({ msg: 'Image Deleted Succesfully' });
+    });
+  } catch (error) {
+    return res.status(500).json({ msg: 'Failed to delete the image', error });
+  }
 });
 
 module.exports = router;
